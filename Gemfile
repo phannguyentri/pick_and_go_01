b0ebe9b56cb4c1d6def929269720955e78dc9d3a
TranDacLam <tdlam2105@gmail.com>
2017-03-29 21:48:25 +0700

    Manage_users

--- Gemfile
+++ Gemfile
@@ -3,8 +3,6 @@ source "https://rubygems.org"
 gem "rails", "~> 5.0.1"
 gem "bcrypt", "3.1.11"
 gem "bootstrap-sass", "3.3.6"
-gem "will_paginate", "3.1.0"
-gem "bootstrap-will_paginate", "0.0.10"
 gem "faker", "1.6.6"
 gem "carrierwave", "0.11.2"
 gem "mini_magick", "4.5.1"
@@ -26,6 +24,8 @@ gem "devise"
 gem "whenever", :require => false
 gem "chatwork"
 gem "font-awesome-sass"
+gem "kaminari"
+gem "bootstrap-kaminari-views"
 group :development, :test do
   gem "pry"
   gem "byebug", platform: :mri
---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url *args
+    [version_name, "no_image.png"].compact.join("_")
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
